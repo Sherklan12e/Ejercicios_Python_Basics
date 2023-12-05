d6033730abfba56199e048eeb6ddc14d4c9006eb
Sherklan <davismendoza.08@gmail.com>
2023-12-05 14:45:17 -0300

    vfvdbdfg

---
+++ Aprende con Alf/Ejercicios de Tipos de Datos Simples/ejercicio5.py
@@ -0,0 +1,7 @@
+#$Escribir un programa que pregunte al usuario por el número de horas trabajadas
+#y el coste por hora. Después debe mostrar por pantalla la paga que le corresponde.
+
+
+gosto = int(input('Cuantas horas trabajaste?: '))
+costo = int(input('Cuanto te pagan por hora?: '))
+print(f'EL monto a pagar de sueldo es de {gosto * costo}')
---
+++ Aprende con Alf/Ejercicios de Tipos de Datos Simples/ejercicio6.py
@@ -0,0 +1,6 @@
+#Escribir un programa que lea un entero positivo, 
+#, introducido por el usuario y después muestre 
+#en pantalla la suma de todos los enteros desde 1 hasta 
+#. La suma de los 
+# primeros enteros positivos puede ser calculada de la siguiente forma:
+
