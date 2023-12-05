3444840d68b8965a357268218030ea082c1a9823
Sherklan <davismendoza.08@gmail.com>
2023-12-05 13:21:24 -0300

    changess

--- Aprende con Alf/Ejercicios de Tipos de Datos Simples/ejercicio3.py
+++ Aprende con Alf/Ejercicios de Tipos de Datos Simples/ejercicio3.py
@@ -1,3 +1,6 @@
 # Escribir un programa que pregunte el nombre del usuario en la consola 
 # y después de que el usuario lo introduzca muestre por pantalla la cadena 
 # ¡Hola <nombre>!, donde <nombre> es el nombre que el usuario haya introducido.
+print('Introduza su nombre porfavor: ')
+nombre =input('x: ')
+print(f'hola {nombre}')
---
+++ Aprende con Alf/Ejercicios de Tipos de Datos Simples/ejercicio4.py
@@ -0,0 +1,4 @@
+# Escribir un programa que muestre por pantalla el resultado de 
+# la siguiente operación aritmética 
+ 
+# .
