c4ca725b39d87a25f3a04a90369d3a5fde8e688e
Sherklan <davismendoza.08@gmail.com>
2023-12-05 14:59:49 -0300

    q

--- Aprende con Alf/cadenas/ejercicio1.py
+++ Aprende con Alf/cadenas/ejercicio1.py
@@ -1,5 +1,4 @@
 # Escribir un programa que pregunte el nombre del usuario en la consola y un número entero e imprima por pantalla en líneas distintas el nombre del usuario tantas veces como el número introducido.
-nombre = input('Escriba su nombre: ')
-numero = int(input('Escriba un numero: '))
-print('nombre ', nombre)
-print('numero ',numero)
+nombre = input("¿Cómo te llamas? ")
+n = input("Introduce un número entero: ")
+print((nombre + "\n") * int(n))
---
+++ Aprende con Alf/cadenas/ejercicio2.py
@@ -0,0 +1,7 @@
+# Escribir un programa que pregunte el nombre completo del usuario en la consola y después muestre por pantalla el nombre completo del usuario tres veces, una con todas las letras minúsculas, otra con todas las letras mayúsculas y otra solo con la primera letra del nombre y de los apellidos en mayúscula. El usuario puede introducir su nombre combinando mayúsculas y minúsculas como quiera.
+
+nombre = input('Escribe tu Fucking nombre perra: ')
+
+print(nombre.lower())
+print(nombre.upper())
+print(nombre.title())
