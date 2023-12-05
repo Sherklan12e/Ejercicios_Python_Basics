139bbe1d2fb904e82b0c490f92ea905b4a74ba72
Sherklan <davismendoza.08@gmail.com>
2023-12-05 14:48:47 -0300

    cha

--- Aprende con Alf/cadenas/ejercicio1.py
+++ Aprende con Alf/cadenas/ejercicio1.py
@@ -1,1 +1,5 @@
 # Escribir un programa que pregunte el nombre del usuario en la consola y un número entero e imprima por pantalla en líneas distintas el nombre del usuario tantas veces como el número introducido.
+nombre = input('Escriba su nombre: ')
+numero = int(input('Escriba un numero: '))
+print('nombre ', nombre)
+print('numero ',numero)
