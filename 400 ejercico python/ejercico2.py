d8db2014be17d4bf9424491d08096214eb857db9
Sherklan <davismendoza.08@gmail.com>
2023-12-28 12:57:41 -0300

    b

---
+++ 400 ejercico python/ejercico2.py
@@ -0,0 +1,7 @@
+primerN = int(input('Ingrese el primer numero: '))
+segundoN= int(input('Ingrese el segundo numero: '))
+
+print(primerN+segundoN)
+print(primerN-segundoN)
+print(primerN/segundoN)
+print(primerN*segundoN)
