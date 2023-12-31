98731375aaf1d4b81ac187d4eb5146d233215593
Sherklan <davismendoza.08@gmail.com>
2023-12-30 21:08:03 -0300

    mayor de edad

---
+++ 400 ejercico python/ejercicio4.py
@@ -0,0 +1,7 @@
+edad = int(input('Escribe tu edad: '))
+if edad>=18:
+    print('Eres Mayor de edad')
+elif edad <18 and edad>=0:
+    print('Eres menor de edad')
+else:
+    print('mm Escribe una edad CTMR')
