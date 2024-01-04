3a8440fbe9ccb505a30078b2702e8dbb69d2a3a0
Sherklan <davismendoza.08@gmail.com>
2024-01-03 23:55:48 -0300

    8

---
+++ 400 ejercico python/ejercicio8.py
@@ -0,0 +1,11 @@
+# Hacer un programa que cuente del 1 al 100 inclusive e imprima los números que son 
+# divisibles por 2 y por 5
+
+
+for i in range( 1+ 100):
+    
+    if i % 2 ==0:
+        print('divisible por 2 = ', i) 
+        
+    if i % 5 ==0:
+        print('divisible por 5 = ', i) 
