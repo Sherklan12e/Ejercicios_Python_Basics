1e9417431bafb966539998de4d436b45412d68e4
Sherklan <davismendoza.08@gmail.com>
2024-01-05 02:50:14 -0300

    12

---
+++ 400 ejercico python/ejercicio12.py
@@ -0,0 +1,11 @@
+# 12)Hacer un programa que muestre el siguiente dibujo         
+# *         
+# *    *          
+# *    *    *          
+# *    *    *     *          
+# *    *    *     *     *
+num = ''
+for i in range(1,5+1):
+    
+    num = '*' + num
+    print(num)
