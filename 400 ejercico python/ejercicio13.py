4854102d1b8627f36e1eecb6748902a4b1681c09
Sherklan <davismendoza.08@gmail.com>
2024-01-05 03:24:13 -0300

    12

---
+++ 400 ejercico python/ejercicio13.py
@@ -0,0 +1,16 @@
+# 13) Idem  anterior con este dibujo          
+# *    *     *     *     *          
+# *    *     *     *              
+# *    *     *          
+# *    *          
+# * 
+# 
+
+
+
+
+num = '*****'
+for i in range(1,5+1):
+    
+    print(num[2])
+    
