9f7d6c1a36c5b467a32262c93887e9736247040e
Sherklan12e <davismendoza.08@gmail.com>
2024-01-10 02:41:20 -0300

    df

--- cursopython/po1.py
+++ cursopython/po1.py
@@ -1,7 +1,26 @@
-# import constant file we created above
-import constant
-# declare constants 
-PI = 3.14
-GRAVITY = 9.8
-print(constant.PI) # prints 3.14
-print(constant.GRAVITY) # prints 9.8
+# #try and exept
+
+
+
+# try:    
+#     edad = int(input('Escribe tu edad: '))
+#     print('la edad del usuario es: ')
+#     input('ingresa una tecla para continuar....') 
+# except KeyboardInterrupt:
+#     print('gracias por eso')
+# except ValueError:
+#     print('Solo enteros plis')
+# hola = 'hasdfasdfasdfasdfasd'
+
+def generador_ejemplo():
+    yield 1
+    yield 2
+    yield 3
+
+# Crear un generador
+mi_generador = generador_ejemplo()
+
+# Obtener valores del generador
+print(next(mi_generador))  # Imprime 1
+print(next(mi_generador))  # Imprime 2
+print(next(mi_generador))  # Imprime 3
