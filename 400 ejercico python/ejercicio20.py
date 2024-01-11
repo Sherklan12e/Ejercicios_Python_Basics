37d94c60664bda605cd197b7590efd4c08bd6b11
Sherklan12e <davismendoza.08@gmail.com>
2024-01-11 19:52:39 -0300

    gs

---
+++ 400 ejercico python/ejercicio20.py
@@ -0,0 +1,5 @@
+# 20)Crear una subrutina llamada “Login”, que recibe un nombre de usuario y una contraseña y 
+# te devuelve Verdadero si el nombre de usuario es “usuario1”      y la contraseña es “asdasd”. Además recibe el número de intentos que se ha intentado 
+# hacer login y si no se ha podido hacer login incremente este valor.      Crear un programa principal donde se pida un nombre de usuario y una contraseña y se 
+# intente hacer login, solamente tenemos tres oportunidades       para intentarlo
+
