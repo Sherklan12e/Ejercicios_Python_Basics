756f724f8c3aaf1cbf947252e9caf49e6a1764e9
Sherklan12e <davismendoza.08@gmail.com>
2024-01-12 06:49:44 -0300

    23

---
+++ 400 ejercico python/ejercicio21.py
@@ -0,0 +1,2 @@
+# 21)Crear una función recursiva que permita calcular el factorial de un número. Realiza un 
+# programa principal donde se lea un entero y se muestre el resultado        del factorial.
