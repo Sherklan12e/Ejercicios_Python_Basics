99164559b90cda03f127401772288dae0639f817
Tonio172112 <toniomurruga@gmail.com>
2024-01-13 02:43:50 -0300

    en realidad estaba este mal

--- Mi Diario Python/ej1/ejercicio7_Tonio172112.py
+++ Mi Diario Python/ej1/ejercicio7_Tonio172112.py
@@ -1,9 +1,9 @@
 #7 - Definir una función es_palindromo() que reconoce palíndromos (es decir, palabras que tienen el mismo aspecto escritas invertidas), ejemplo: es_palindromo ("radar") tendría que devolver True.
 def is_palindromo(cad):
-    cad=cad.upper().strip()
+    cad=cad.upper().replace(" ","")
     if(cad==cad[::-1]):
         return True
     else:
         return False
     
-print(is_palindromo("Ana"))
+print(is_palindromo("yo de todo te doy"))
