aad5aa35e444afef28e72625cd86355ff8fe99d6
Sherklan12e <davismendoza.08@gmail.com>
2024-01-22 05:15:27 -0300

    lista#3

--- Mi Diario Python/listas/3_sherklan12e.py
+++ Mi Diario Python/listas/3_sherklan12e.py
@@ -1,19 +1,19 @@
-from typing import List 
+from typing import List , Union
 
-def ordenada(lista: List )-> bool:
+def ordenada(lista: Union[List[str], List[int]]) -> bool:
     
-    for i in range(len(lista) - 1):
-        if type(lista[i]) != type(lista[i + 1]):
-            
+    for i in range(len(lista) -1):
+        
+        
+        if lista[i] > lista[i+1]:
             return False
-        if lista[i] > lista[i + 1]:
-            
-            return False
+        
     return True
     
 
-lista_int = [1,2,5,2,65,1,6]
-lista_str = ['a','f','g','h']
+lista_int = [1,2,5]
+lista_str = ['s','a','g','h']
 
 
 print(ordenada(lista_str))
+print(ordenada(lista_int))
