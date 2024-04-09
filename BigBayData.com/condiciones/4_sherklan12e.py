ca6b9d77809d566806a9b88ccbea94241f0f2fd7
Sherklan12e <davismendoza.08@gmail.com>
2024-04-09 04:01:38 -0300

    ejercicio4

---
+++ BigBayData.com/condiciones/4_sherklan12e.py
@@ -0,0 +1,17 @@
+num1 = int(input("numero 1: "))
+num2 = int(input("numero 2: "))
+print("""
+    1) multiplicar
+    2) dividir
+    3) sumar
+    4) restar
+    """)
+opcion = int(input("Elije la opcion: "))
+if opcion==1:
+    print(num1*num2)
+if opcion==2:
+    print(num1/num2)
+if opcion==1:
+    print(num1+num2)
+if opcion==1:
+    print(num1-num2)
