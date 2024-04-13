5355cb03eeadf35e1d943cd4efb5f266c0914870
Sherklan12e <davismendoza.08@gmail.com>
2024-04-13 09:27:48 -0300

    8_resolve

--- BigBayData.com/condiciones/8_sherklan12e.py
+++ BigBayData.com/condiciones/8_sherklan12e.py
@@ -3,3 +3,9 @@
 # Si el año es uniformemente divisible por 4, vaya al paso 2. De lo contrario, vaya al paso 5. Si el año es uniformemente divisible por 100, vaya al paso 3. De lo contrario, vaya al paso 4. Si el año es uniformemente divisible por 400, vaya al paso 4. De lo contrario, vaya al paso 5. El año es un año bisiesto (tiene 366 días). El año no es un año bisiesto (tiene 365 días).
 
 
+year = int(input("Enter year here: "))
+
+if (year % 4== 0 and year % 100 !=0) or  (year % 400 ==0):
+    print("the year is bisiesto")
+else:
+    print("the year is not bisiesto!")
