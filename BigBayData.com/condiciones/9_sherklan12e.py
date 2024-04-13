5a1d44d08bd3ee31b8262b9022194db50ced4cb9
Sherklan12e <davismendoza.08@gmail.com>
2024-04-13 09:31:15 -0300

    polideportivo

---
+++ BigBayData.com/condiciones/9_sherklan12e.py
@@ -0,0 +1,3 @@
+# Partiendo de la tarifa anual (que puede cambiar), nos piden que debemos calcular el precio de la tarifa de nuestro polideportivo, sabiendo las siguientes condiciones:
+
+# Criterio 1: Si es mayor de edad y está trabajando -> Paga el 100% Criterio 2: Si es menor de edad y está trabajando -> Paga el 95% Criterio 3: Si es mayor de edad y no está trabajando -> Paga el 75% Criterio 4: Si es menor de edad y no está trabajando -> Paga el 50%
