d38d0983ed261df5a938f85c9fa206cdbe659973
sherklan12e <davismendoza.08@gmail.com>
2024-04-17 22:15:19 -0300

    ejercicio4

---
+++ BigBayData.com/bucles/4sherklan12e.py
@@ -0,0 +1,5 @@
+# Crea un algoritmo para la sucesión de Fibonacci. La sucesión de Fibonacci es la siguiente serie:
+
+#  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
+
+# Pista: Empezando por 0 y 1, el siguiente número es la suma de los dos números últimos.
