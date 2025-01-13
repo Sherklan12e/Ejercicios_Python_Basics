905c25af62617307a66d92ddab3a703c8ed2cfe0
Sherklan <97853791+Sherklan12e@users.noreply.github.com>
2025-01-13 07:14:48 -0300

    ejercicio20

--- 400 ejercico python/ejercicio20.py
+++ 400 ejercico python/ejercicio20.py
@@ -2,4 +2,29 @@
 # te devuelve Verdadero si el nombre de usuario es “usuario1”      y la contraseña es “asdasd”. Además recibe el número de intentos que se ha intentado 
 # hacer login y si no se ha podido hacer login incremente este valor.      Crear un programa principal donde se pida un nombre de usuario y una contraseña y se 
 # intente hacer login, solamente tenemos tres oportunidades       para intentarlo
+def Login(username , password , attempts):
+    if username == "useruario1" and password == "asdaasd":
+        return True
 
+    return False, attempts + 1
+
+def main():
+    max_attempts = 3
+    attempts = 0 
+
+    while attempts < max_attempts:
+        username = input("Ingrese su nombre: ")
+        password = input("Ingrese su contraseña: ")
+
+        success, attempts = Login(username,password, attempts)
+        if success:
+            print("Logeado con exito")
+            break
+        else:
+            remaining = max_attempts - attempts
+            print(f"error , {remaining} intentos restantes")
+    if attempts >= max_attempts:
+        print("Te pasaste de intentos")
+
+if __name__ == '__main__':
+    main()
