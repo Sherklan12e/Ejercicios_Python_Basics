a1ae521aaa96fb136d830bfa3b418adb0fd35ce4
Sherklan12e <davismendoza.08@gmail.com>
2025-03-09 22:03:22 -0300

    ejercicio21

--- 400 ejercico python/ejercicio21.py
+++ 400 ejercico python/ejercicio21.py
@@ -1,3 +1,14 @@
 # 21)Crear una función recursiva que permita calcular el factorial de un número. Realiza un 
-# programa principal donde se lea un entero y se muestre el resultado        del factorial.
+# programa principal donde se lea un entero y se muestre el resultado del factorial.
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+if __name__ == "__main__":
+    num = int(input("Introduce un número entero: "))
+    print(f"El factorial de {num} es {factorial(num)}")
+    
 #Esto es una prueba
